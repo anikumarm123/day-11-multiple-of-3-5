aa76337035504f109e57050bb9f2afbae4c971f2
muthukumar <anikumar2825@gmail.com>
2022-06-24 09:51:26 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,10 @@
+// range of mulptiple of 3 and 5 //
+n = 50;
+for(i=0;i<=n;i++){
+    if(i%3==0){
+console.log("Multiple of 3  :  "+i);
+    }
+    else if(i%5==0){
+console.log("Multiple of 5  :  "+i);
+    }
+}
